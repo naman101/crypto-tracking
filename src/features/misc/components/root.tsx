59de957e751ecff85361d7bb7a62ec6291992804
Naman Agarwal <naman305@outlook.com>
2024-10-27 19:17:43 +0530

    fix: header link to home page

--- src/features/misc/components/root.tsx
+++ src/features/misc/components/root.tsx
@@ -1,12 +1,14 @@
-import { Outlet } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
 
 export function Root() {
   return (
     <>
-      <nav className="bg-white shadow tw-flex sticky top-0">
-        <h1 className="text-blue-500 text-b px-2 py-4 md:px-3 md:py-6 lg:px-6 lg:py-8 lg:text-2xl">
-          CryptoTracker
-        </h1>
+      <nav className="bg-white shadow sticky top-0">
+        <Link to="/">
+          <h1 className="text-blue-500 text-b px-2 py-4 md:px-3 md:py-6 lg:px-5 lg:py-7 lg:text-2xl">
+            CryptoTracker
+          </h1>
+        </Link>
       </nav>
       <div className="p-2 md:p-4 lg:p-6 xl:p-8 space-y-3 font-sans min-h-screen">
         <Outlet />
